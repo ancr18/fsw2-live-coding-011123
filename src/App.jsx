a1c760799086611764cad6b784306d003a3991a8
Ardiyansyah R <arsk9797@gmail.com>
2023-11-01 22:56:43 +0800

    menambahkan react reducer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import CartContextProvider from "./store/shopping-cart-context.jsx";
+// import { CartContext } from "./store/shopping-cart-context.jsx";
+
+function App() {
+  return (
+    <CartContextProvider>
+      <Header />
+      <Shop />
+    </CartContextProvider>
+  );
+}
+
+export default App;
